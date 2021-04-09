85e34e7127e56686a3ed7310dad50389babcc032
UN10 <62202857+UanTan123@users.noreply.github.com>
2021-04-09 20:09:37 +0900

    Update whisper.kt

--- src/main/kotlin/un10/un10/Commands/whisper.kt
+++ src/main/kotlin/un10/un10/Commands/whisper.kt
@@ -14,21 +14,13 @@ class whisper : CommandExecutor
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean
     {
         val w: Player = Bukkit.getServer().getPlayer(args[0]) as Player
-
-        if (w == Bukkit.getOfflinePlayer(args[0]))
+        val str = StringBuilder()
+        for (i in 1 until args.count())
         {
-            sender.sendMessage(loge.chatFormat("&c오프라인 플레이어는 귓속말을 할 수 없습니다!"))
+            str.append(args[i] + " ")
         }
-        else
-        {
-            val str = StringBuilder()
-            for (i in 1 until args.count())
-            {
-                str.append(args[i] + " ")
-            }
-            val msg = str.toString()
-            w.sendMessage("&7${sender.name}로 부터 귓속말이 도착했습니다 : $msg")
-        }
+        val msg = str.toString()
+        w.sendMessage("&7${sender.name}로 부터 귓속말이 도착했습니다 : $msg")
 
         return true
     }
